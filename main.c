b4ab2a6ea4b084f1b4ffd44608e21633262b5774
Nicolas BOISDE <nicolasboisde@gmail.com>
2020-11-15 00:09:51 +0100

    main: (sleep function + authors.)

--- main.c
+++ main.c
@@ -4,29 +4,36 @@
 int		main()
 {
 	printf(BLUE);
+	printf("WELCOME IN THE LIBFT_TESTS\nWritten by ndemont and nboisde, enjoy the defense.\n");
+
+	sleep(1);
 	printf("\n%s\n", "TEST FOR MEMSET");
 	printf(DEFAULT);
 	test_memset();
 	printf("\n");
 
+	sleep(1);
 	printf(BLUE);
 	printf("%s\n", "TEST FOR BZERO");
 	printf(DEFAULT);
 	test_bzero();
 	printf("\n");
 
+	sleep(1);
 	printf(BLUE);
 	printf("%s\n", "TEST FOR MEMCPY");
 	printf(DEFAULT);
 	test_memcpy();
 	printf("\n");
 
+	sleep(1);
 	printf(BLUE);
 	printf("%s\n", "TEST FOR MEMCCPY");
 	printf(DEFAULT);
 	test_memccpy();
 	printf("\n");
 
+	sleep(1);
 	printf(BLUE);
 	printf("%s\n", "TEST FOR MEMMOVE");
 	printf(DEFAULT);
